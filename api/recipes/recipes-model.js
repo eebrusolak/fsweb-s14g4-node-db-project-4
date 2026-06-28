441d804ce6aec261288212388813a9565b9e6e04
eebrusolak <70767204+eebrusolak@users.noreply.github.com>
2026-06-29 02:03:15 +0300

    Update recipes-model.js

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -10,9 +10,13 @@ async function idyeGoreTarifGetir(recipe_id) {
         if(!recipe) return null;
 
         const steps = await db("steps")
-            .where("recipe_id", recipe_id)
-            .orderBy("step_number");
-
+    .select(
+        "step_id",
+        "step_number",
+        "instructions"
+    )
+    .where("recipe_id", recipe_id)
+    .orderBy("step_number");
 
         for(const step of steps) {
             step.ingredients = await db("step_ingredients")
